f7a80d3a329b63750c65646b71ba3dedf2be2007
Taptiyga <48513295+taptiyga@users.noreply.github.com>
2025-01-08 15:54:41 +0400

    result2?

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function Exponentiation(a, b) {
-    if (typeof (a && b) === 'number' && b >= 0) {
+    if (typeof a === 'number' && typeof b === 'number' && b >= 0) {
         let sum = 1
         for (let index = 0; index < b; index++) {
             sum = sum * a
@@ -12,4 +12,20 @@ function Exponentiation(a, b) {
     }
 }
 
-Exponentiation(3, 0)
+Exponentiation(3, 10)
+
+
+function ExponentiationSum(a, b) {
+    if (typeof a === 'number' && typeof b === 'number' && b >= 0) {
+        let sum = a
+        for (let index = 0; index < b; index++) {
+            sum += sum
+        }
+        return sum
+    } else {
+        console.log(`error:оба параметра должны быть числами, второе значение не может быть
+                отрицательным`)
+    }
+}
+let result = ExponentiationSum(4, 0)
+console.log(result)
