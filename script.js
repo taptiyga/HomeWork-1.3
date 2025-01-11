46f1a32a498dcd4ba56cf9235db2621cd7b07cb4
Taptiyga <48513295+taptiyga@users.noreply.github.com>
2025-01-11 13:07:37 +0400

    commets

--- script.js
+++ script.js
@@ -19,13 +19,16 @@ function ExponentiationSum(a, b) {
     if (b > 0 && typeof a === "number" && typeof b === "number") {
         let result = a;
         for (let i = 1; i < b; i++) {
+            console.log(`Внешний цикл ${i}`)
             let temp = 0;
+            console.log(`Вложенный цикл пройдет ${result} раз`)
             for (let j = 0; j < result; j++) {
                 temp += a;
+                console.log(temp)
             }
             result = temp;
         }
-        console.log(result)
+        console.log(`Ответ ${result}`)
     } else if (b === 0) {
         console.log(1)
     } else if (b < 0) {
@@ -35,4 +38,4 @@ function ExponentiationSum(a, b) {
     }
 }
 
-ExponentiationSum(3, 4)
+ExponentiationSum(3, 5)
